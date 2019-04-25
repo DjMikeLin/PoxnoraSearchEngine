32cd9659117850a9cfc14a6bb64b21f275806791
MikeLin <linmichael97@yahoo.com>
2019-04-25 17:44:29 -0400

    Working...

--- controllers/champController.js
+++ controllers/champController.js
@@ -3,6 +3,17 @@ const Champion = require('../models/champ.js');
 const champController = {
     show: (req, res) => {
         Champion.find().then(champs => {
+
+/*         let abilities = champs.map(champ => {
+            champ.abilitySets.map(abilitySet => {
+                console.log(abilitySet);
+                abilitySet.abilities.map(ability => {
+                    return ability.name;
+                })
+            })
+        }) */
+        //console.log(champs[0].abilitySets[0][0].abilities[1].name);
+
             res.render("runes/champ", { champs });
         }).catch(error => {
             console.log(error);

3f9ca4099e4563197fd07814c89b15063223f564
MikeLin <linmichael97@yahoo.com>
2019-05-04 18:09:06 -0400

    1. Removed some irrelevant full crud stuff 2. Added in lazy loading for images

--- routes/index.js
+++ routes/index.js
@@ -10,42 +10,18 @@ router.get('/', searchController.show);
 //Routes for spells
 router.get('/spells', spellController.show);
 router.post('/spells', spellController.filter);
-/* router.post('/spells', spellController.create);
-router.get('/spells/error', searchController.creationError);
-router.get('/spells/create', spellController.createPage); */
 router.get('/spells/:id', spellController.getRune);
-/* router.put('/spells/:id', spellController.edit);
-router.delete('/spells/:id', spellController.delete);
-router.get('/spells/:id/edit', spellController.getRuneToEdit); */
 //Routes for champions
 router.get('/champions', champController.show);
 router.post('/champions', champController.filter);
-/*router.post('/champions', champController.create);
-router.get('/champions/error', searchController.creationError);
-router.get('/champions/create', champController.createPage);*/
 router.get('/champions/:id', champController.getRune);
-/* router.put('/champions/:id', champController.edit);
-router.delete('/champions/:id', champController.delete);
-router.get('/champions/:id/edit', champController.getRuneToEdit); */
 //Routes for relics
 router.get('/relics', relicController.show);
 router.post('/relics', relicController.filter);
-/* router.post('/relics', relicController.create);
-router.get('/relics/error', searchController.creationError);
-router.get('/relics/create', relicController.createPage); */
 router.get('/relics/:id', relicController.getRune);
-/* router.put('/relics/:id', relicController.edit);
-router.delete('/relics/:id', relicController.delete);
-router.get('/relics/:id/edit', relicController.getRuneToEdit); */
 //Routes for equipments
 router.get('/equipments', equipController.show);
 router.post('/equipments', equipController.filter);
-//router.post('/equipments', equipController.create);
-/* router.get('/equipments/error', searchController.creationError);
-router.get('/equipments/create', equipController.createPage); */
 router.get('/equipments/:id', equipController.getRune);
-/* router.put('/equipments/:id', equipController.edit);
-router.delete('/equipments/:id', equipController.delete);
-router.get('/equipments/:id/edit', equipController.getRuneToEdit); */
 
 module.exports = router;

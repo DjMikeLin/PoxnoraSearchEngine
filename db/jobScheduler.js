5048f6339f6dcd45480ee986a5dc2e1fa4809e5e
DjMikeLin <linmichael97@yahoo.com>
2020-11-30 21:30:23 -0500

    Testing scheduler5

--- db/jobScheduler.js
+++ db/jobScheduler.js
@@ -1,15 +1,14 @@
 const CronJob = require('cron').CronJob;
 const PostPatch = require('./postPatch');
 
-module.exports = {
-    job: new CronJob(
-        '0 3 * * *', 
-        () => {
-            PostPatch.getJSON()
-        },
-        null,
-        false, 
-        'America/New_York'
-    )
-};
-//testing
+const job = new CronJob(
+    '0 3 * * *', 
+    () => {
+        PostPatch.getJSON()
+    },
+    null,
+    false, 
+    'America/New_York'
+)
+
+job.start();
